4c6832309bc9cbbdaf0e180ffcc9a09a6a382500
Patrick Kim <patrick.kim408@gmail.com>
2017-08-07 11:07:17 -0800

    restcontroller and requestmapping

--- src/main/java/com/patrick/controllerspractice/controllers/HomeController.java
+++ src/main/java/com/patrick/controllerspractice/controllers/HomeController.java
@@ -2,10 +2,14 @@ package com.patrick.controllerspractice.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-
+@RequestMapping("/hello")
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping("")
 	public String home() {
 		return "Hello World!";
+	}
+	@RequestMapping("/world")
+	public String world() {
+		return "Class level annotations are cool too!";
 	}
 }

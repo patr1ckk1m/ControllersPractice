6e818928d622d708a5f82dacbccf14c05702aafd
Patrick Kim <patrick.kim408@gmail.com>
2017-08-07 11:00:39 -0800

    first commit

---
+++ src/main/java/com/patrick/controllerspractice/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.patrick.controllerspractice.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+}
